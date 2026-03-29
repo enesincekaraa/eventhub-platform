640068cbe460a63064a7156604f5fdcbf6e128bf
enesincekaraa <enesincekara61@gmail.com>
2026-03-29 19:18:06 +0300

    Initial project setup

---
+++ src/test/java/com/enesincekara/eventhub/EventhubPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesincekara.eventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhubPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
