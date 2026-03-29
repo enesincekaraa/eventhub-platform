0866cbc0d520ad88d9d1549e11fb6e2eccdbcf91
enesincekaraa <enesincekara61@gmail.com>
2026-03-29 19:18:06 +0300

    Initial project setup

---
+++ src/main/java/com/enesincekara/eventhub/EventhubPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.enesincekara.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventhubPlatformApplication.class, args);
+    }
+
+}
